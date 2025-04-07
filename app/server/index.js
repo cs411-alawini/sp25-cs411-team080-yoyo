07fc5721d99b236c65b78ac1a07a97bee0f947f1
linguanhong <gl48@illinois.edu>
2025-04-07 14:27:15 -0500

    create the homepage

--- app/server/index.js
+++ app/server/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const userRoutes = require('./routes/userRoute');
 const carRoutes = require('./routes/carRoute');
+const carController = require('./Controller/carController');
 const path = require('path');
 
 const app = express()//create a express application
 app.set('view engine', 'ejs');
+// Serve static files from the public folder
+app.use(express.static(path.join(__dirname, '..', 'public')));
 app.set('views', path.join(__dirname, './Views'));
 
 app.use('/user', userRoutes);
@@ -21,3 +24,4 @@ app.get('/login', (req, res)=>{
 app.listen(3000, ()=>{
     console.log('listen on port 3000')
 })
+
